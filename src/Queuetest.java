759c50e93c3d16d5f2881fd2fcd4a770b4752218
Chandupa Dilhana <chandupadilhan04@gmail.com>
2025-12-03 19:00:11 +0530

    fill methods

--- src/Queuetest.java
+++ src/Queuetest.java
@@ -3,6 +3,18 @@ import ds.Queue;
 public class Queuetest {
     public static void main(String[] args) {
         Queue queue=new Queue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.print();
+        queue.dequeue();
+        queue.print();
+        queue.dequeue();
+        queue.print();
+        queue.peek();
+        queue.print();
+        queue.clear();
+        queue.size();
 
 
     }
--- src/ds/Queue.java
+++ src/ds/Queue.java
@@ -1,23 +1,66 @@
 package ds;
 
 public class Queue {
+
+    private int[] numbers;
+
     public void enqueue(int num){
+        if (numbers==null){
+            int[] temp=new int[1];
+            temp[0]=num;
+            numbers=temp;
+        }else{
+            int[] temp =new int[numbers.length+1];
+            for(int i=0;i<numbers.length;i++){
+                temp[i]=numbers[i];
+            }
+            temp[temp.length-1]=num;
+            numbers=temp;
+        }
+
 
     }
     public void dequeue(){
+        if (empty()){
+            System.out.println("cannot dequeue from empty array");
+            return;
+
+        }else if(numbers.length==1){
+            numbers=null;
+            return;
+        }else{
+            int[] temp=new int[numbers.length-1];
+            for(int i=1;i< numbers.length;i++){
+                temp[i-1]=numbers[i];
+            }
+            numbers=temp;
+
+        }
 
     }
     public boolean empty(){
-        return false;
+        return numbers==null;
 
     }
     public int peek(){
-       return 0;
+       return numbers==null?-1:numbers[0];
     }
     public void clear(){
+        numbers=null;
 
     }
     public void print(){
+        System.out.println();
+        if(empty()){
+            System.out.println("[]");
+        }else{
+            System.out.print("[");
+            for(int i=numbers.length-1;i>=0;i--){
+                System.out.print(numbers[i]+" ,");
+            }
+            System.out.print("\b\b]");
+        }
+
 
     }
     public boolean contains(int num){
@@ -25,7 +68,7 @@ public class Queue {
 
     }
     public int size(){
-        return 0;
+        return numbers==null?0: numbers.length;
 
     }
 
