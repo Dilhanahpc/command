ee9d1d9a8df5c621b438d14db249b5668e488ec4
Chandupa Dilhana <chandupadilhan04@gmail.com>
2025-12-03 11:30:13 +0530

    Create class structure

---
+++ src/ds/Stack.java
@@ -0,0 +1,20 @@
+package ds;
+
+public class Stack {
+
+    public void push(){
+
+    }
+    public void pop(){
+
+    }
+    public boolean empty(){
+        return false;
+    }
+    public int size(){
+        return 0;
+    }
+    public int peek(){
+        return 0;
+    }
+}
